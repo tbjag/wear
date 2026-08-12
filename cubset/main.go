69c9756c1a939b836e954334dd2317b21d000db7
tbjag <tbjagodits@gmail.com>
2026-08-12 14:31:47 -0700

    lexing

--- cubset/main.go
+++ cubset/main.go
@@ -3,18 +3,80 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 )
 
+type TokenType int
+
+const (
+	EOF TokenType = iota
+	Print
+	Put
+	While
+	If
+	Else
+	LeftParen
+	RightParen
+	StringLiteral
+)
+
+type TokenMatch struct {
+	tokenType TokenType
+	regex     *regexp.Regexp
+}
+
+type Token struct {
+	tokenType TokenType
+	val     string
+}
+
+var TokenMatches = []TokenMatch{
+	{Print, regexp.MustCompile(`\bprint\b`)},
+	{Put,   regexp.MustCompile(`\bput\b`)},
+	{While,  regexp.MustCompile(`\bwhile\b`)},
+	{StringLiteral, regexp.MustCompile(`"[^"\\]*(\\.[^"\\]*)*"`)},
+	{LeftParen, regexp.MustCompile(`\(`)},
+	{RightParen, regexp.MustCompile(`\)`)},
+	{If, regexp.MustCompile(`if`)},
+}
+
+
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func main() {
-	data, err := os.ReadFile("tests/hello_world.txt")
+	content, err := os.ReadFile("tests/hello_world.txt")
 	check(err)
 
-	fmt.Printf("file size %d\n", len(data))
-	fmt.Printf("file content : %s\n", data)
+	data := string(content)
+
+	re := regexp.MustCompile(`\bprint\b`)
+	result := re.FindStringIndex(string(content))
+
+	fmt.Println("found print: ", result)
+
+	var res[]Token
+
+	for {
+		// match on the first index 0
+		for _, tokenMatch := range TokenMatches {
+			m := tokenMatch.regex.FindStringIndex(data)
+			if m != nil {
+				if m[0] != 0 {
+					continue
+				}
+				res = append(res, Token{tokenMatch.tokenType, ""})
+			}
+			break
+		}
+		// chop by size of string matched
+		// create token here
+		// if we dont find a match then we error
+		// keep chopping until EOF, append eof
+		break
+	}
+	fmt.Printf("%v", res)
 }
