f6154834d1915f2eab8452839ec042cad8ebd18a
tbjag <tbjagodits@gmail.com>
2026-09-04 14:41:13 -0700

    working mvp!

---
+++ payout/readme.md
@@ -0,0 +1,8 @@
+# todos
+
+- convert to i64 cents when doing calculations
+- convert tuple to struct for readability
+- integrate with clap for cli
+  - start new session
+  - pull up saved sessions (db?)
+- better printing
--- payout/src/main.rs
+++ payout/src/main.rs
@@ -1,4 +1,5 @@
 use std::{collections::HashMap, io::{self, Write}};
+use std::cmp;
 // use clap::{Parser};
 
 // #[derive(Parser)]
@@ -60,15 +61,12 @@ fn calc_net_owed(paid_in: HashMap<String, f32>) -> HashMap<String, f32> {
         .map(|(person, amount)| (person.clone(), amount - fair_share))
         .collect();
     println!("{net_paid_in:?}");
-    paid_in
+    net_paid_in
 }
 
-fn check_net_zero(l: Vec<(String, f32)>) -> bool {
-    todo!()
-} 
 
-fn greedy_min_cash_flow(net_paid_in: HashMap<String, f32>) {
-    let mut debtors: Vec<(String, f32)> = net_paid_in
+fn greedy_min_cash_flow(net_owed: HashMap<String, f32>) {
+    let mut debtors: Vec<(String, f32)> = net_owed
         .iter()
         .filter(|&(_, &net_amount)| net_amount < 0.0)
         .map(|(name, &net_amount)| (name.clone(), -net_amount))
@@ -76,7 +74,7 @@ fn greedy_min_cash_flow(net_paid_in: HashMap<String, f32>) {
 
     debtors.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
 
-    let mut creditors: Vec<(String, f32)> = net_paid_in
+    let mut creditors: Vec<(String, f32)> = net_owed
         .iter()
         .filter(|&(_, &net_amount)| net_amount > 0.0)
         .map(|(name, &net_amount)| (name.clone(), net_amount))
@@ -84,7 +82,29 @@ fn greedy_min_cash_flow(net_paid_in: HashMap<String, f32>) {
 
     creditors.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
 
-    while 
+    debtors.retain(|x| x.1 > 0.01);
+    creditors.retain(|x| x.1 > 0.01);
+    
+    while debtors.len() > 0 && creditors.len() > 0 {
+        let biggest_creditor = creditors.get(0).expect("none found");
+        let biggest_debtor = debtors.get(0).expect("none found");
+
+        let debtor_pays = biggest_creditor.1.min(biggest_debtor.1);
+        let biggest_debtor = biggest_debtor.0.clone();
+        let biggest_creditor = biggest_creditor.0.clone();
+        println!("{biggest_debtor} pays {biggest_creditor} {debtor_pays}");
+
+        creditors[0].1 -= debtor_pays;
+        debtors[0].1 -= debtor_pays;
+
+        debtors.retain(|x| x.1 > 0.01);
+        creditors.retain(|x| x.1 > 0.01);
+
+        debtors.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+        creditors.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+    }
+
+    println!("DONE");
 }
 
 fn main() {
@@ -99,7 +119,7 @@ fn main() {
 
     let paid_in = get_amounts();
     let net_owed= calc_net_owed(paid_in);
-
+    greedy_min_cash_flow(net_owed);
     
     
 }
